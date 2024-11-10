e32078ddfc92aa1f61ac95188dcfaff321df3dd9
test-ilya <asdnone@yandex.ru>
2024-11-10 13:29:17 +0300

    modified:   playwright.config.ts 	modified:   src/tests/searchingItem.test.ts

--- src/tests/searchingItem.test.ts
+++ src/tests/searchingItem.test.ts
@@ -6,9 +6,13 @@ const itemName = 'MacBook Air 13';
 test('Searching item', async ({ page }) => {
     const mainPage = new MainPage(page);
 
+    await page.screenshot({ path: 'screenshot.png', fullPage: true });
     await mainPage.openMainPage();
+    await page.screenshot({ path: 'screenshot.png', fullPage: true });
     await mainPage.inputInSearchString(itemName);
+    await page.screenshot({ path: 'screenshot.png', fullPage: true });
     await mainPage.clickSearchBtn();
+    await page.screenshot({ path: 'screenshot.png', fullPage: true });
 
     await expect(
         page.locator(mainPage.serpListSelector)
