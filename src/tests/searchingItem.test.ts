f54e3d01b05a14e7d02aedb0f892b2259a2a347f
test-ilya <asdnone@yandex.ru>
2024-11-10 13:35:12 +0300

    modified:   playwright-report/index.html 	modified:   src/tests/searchingItem.test.ts

--- src/tests/searchingItem.test.ts
+++ src/tests/searchingItem.test.ts
@@ -6,13 +6,9 @@ const itemName = 'MacBook Air 13';
 test('Searching item', async ({ page }) => {
     const mainPage = new MainPage(page);
 
-    await page.screenshot({ path: 'screenshot.png', fullPage: true });
     await mainPage.openMainPage();
-    await page.screenshot({ path: 'screenshot2.png', fullPage: true });
     await mainPage.inputInSearchString(itemName);
-    await page.screenshot({ path: 'screenshot3.png', fullPage: true });
     await mainPage.clickSearchBtn();
-    await page.screenshot({ path: 'screenshot4.png', fullPage: true });
 
     await expect(
         page.locator(mainPage.serpListSelector)
